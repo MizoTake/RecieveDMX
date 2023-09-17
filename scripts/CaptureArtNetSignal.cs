2c9c925ec199a986381576c5514ffa615a63500a
MizoTake <mizotake123@gmail.com>
2023-09-17 20:06:13 +0900

    impl: dmx信号を受け取れる処理

---
+++ scripts/CaptureArtNetSignal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using Godot;
+using LXProtocols.Acn.Rdm;
+using LXProtocols.Acn.Sockets;
+using LXProtocols.ArtNet;
+using LXProtocols.ArtNet.Packets;
+using LXProtocols.ArtNet.Sockets;
+
+namespace GodotProject.scripts;
+
+public class CaptureArtNetSignal : IDisposable
+{
+
+    private const uint DmxLengthPerUniverse = 512;
+    
+    private readonly ArtNetSocket socket = new (UId.Empty);
+    private byte[] dmxData;
+
+    public delegate void NotifyNewPacket(in byte[] signals);
+    public event NotifyNewPacket OnNotifySignals;
+    
+    public CaptureArtNetSignal(IPAddress ipAddress, IPAddress subnetMaskAddress, uint universe)
+    {
+        dmxData = new byte[DmxLengthPerUniverse * universe];
+
+        socket.NewPacket += SocketOnNewPacket;
+        socket.UnhandledException += SocketOnUnhandledException;
+
+        socket.Open(ipAddress, subnetMaskAddress);
+    }
+
+    private void SocketOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        var exception = e.ExceptionObject as Exception;
+        GD.PrintErr(exception);
+    }
+
+    private void SocketOnNewPacket(object sender, NewPacketEventArgs<ArtNetPacket> e)
+    {
+        switch (e.Packet.OpCode) {
+            case ArtNetOpCodes.Dmx:
+                var dmxPacket = e.Packet as ArtNetDmxPacket;
+                Array.Copy(dmxPacket.DmxData, 0, dmxData, DmxLengthPerUniverse * dmxPacket.Universe, DmxLengthPerUniverse);
+                OnNotifySignals?.Invoke(dmxData);
+                break;
+        }
+    }
+
+    void IDisposable.Dispose()
+    {
+        socket.UnhandledException -= SocketOnUnhandledException;
+        socket.NewPacket -= SocketOnNewPacket;
+        socket.Close();
+        socket.Dispose();
+    }
+}
---
+++ scripts/DMXReciever.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Net;
+using System.Text;
+using Godot;
+using GodotProject.scripts;
+
+public partial class DMXReciever : Node
+{
+
+	[Export]
+	private string ipAddressString = "127.0.0.1";
+	[Export]
+	private string subnetMaskAddressString = "255.0.0.0";
+	[Export]
+	private uint universe = 4;
+
+	private byte[] signals;
+	private StringBuilder stringBuilder = new();
+	private CaptureArtNetSignal captureArtNetSignal;
+
+	public override void _Ready()
+	{
+		var ipAddress = IPAddress.Parse(ipAddressString);
+		var subnetMaskAddress = IPAddress.Parse(subnetMaskAddressString);
+		captureArtNetSignal = new CaptureArtNetSignal(ipAddress, subnetMaskAddress, universe);
+		captureArtNetSignal.OnNotifySignals += OnNotifySignals;
+	}
+
+	private void OnNotifySignals(in byte[] signals)
+	{
+		this.signals = signals;
+		stringBuilder.Clear();
+		for (var i = 0; i < this.signals.Length; i++)
+		{
+			stringBuilder.AppendLine($"{i}: {signals[i]}");
+		}
+		GD.Print(stringBuilder.ToString());
+	}
+
+	public override void _ExitTree()
+	{
+		captureArtNetSignal.OnNotifySignals -= OnNotifySignals;
+		(captureArtNetSignal as IDisposable).Dispose();
+	}
+}
